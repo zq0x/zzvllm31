990677ade945af72216a8660f0fb1b7cee6026ab
y <yqwx01z@gmail.com>
2025-03-30 16:58:33 +0200

    commit

--- Dockerfile
+++ Dockerfile
@@ -14,16 +14,4 @@ RUN pip3 install huggingface_hub vllm fastapi uvicorn pynvml
 
 COPY . .
 
-EXPOSE 1370
-EXPOSE 1371
-EXPOSE 1372
-EXPOSE 1373
-EXPOSE 1374
-EXPOSE 1375
-EXPOSE 1376
-EXPOSE 1377
-EXPOSE 1378
-EXPOSE 1379
-EXPOSE 1380
-
 CMD ["python", "app.py", "--model", "Qwen/Qwen2.5-1.5B-Instruct", "--tensor-parallel-size", "1", "--gpu-memory-utilization", "0.92", "--max-model-len", "2048", "--port", "1370"]
--- app.py
+++ app.py
@@ -7,56 +7,44 @@ import pynvml
 from vllm import LLM, SamplingParams
 from transformers import AutoModelForCausalLM
 import gc
-import logging
 import argparse
-
-LOG_PATH= './logs'
-LOGFILE_CONTAINER = './logs/logfile_container_vllm.log'
-os.makedirs(os.path.dirname(LOGFILE_CONTAINER), exist_ok=True)
-logging.basicConfig(filename=LOGFILE_CONTAINER, level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] started logging in {LOGFILE_CONTAINER}')
-
 
 
 def initialize_nvml():
     try:
         pynvml.nvmlInit()
-        logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [initialize_nvml] NVML initialized successfully.')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [initialize_nvml] NVML initialized successfully.')
     except Exception as e:
-        logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [initialize_nvml] Failed to initialize NVML: {e}')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [initialize_nvml] Failed to initialize NVML: {e}')
 
 initialize_nvml()
 
-# Check if CUDA is available
+
 def cuda_support_bool():
     try:
         res_cuda_support = torch.cuda.is_available()
         if res_cuda_support:
-            logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] CUDA found')
+            print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] CUDA found')
         else:
-            logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] CUDA not supported!')
+            print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] CUDA not supported!')
         return res_cuda_support
     except Exception as e:
-        logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] {e}')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] {e}')
         return False
 
-# Return the number of CUDA GPUs available
+
 def cuda_device_count():
     try:
         res_gpu_int = torch.cuda.device_count()
         res_gpu_int_arr = [i for i in range(res_gpu_int)]
-        logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] {str(res_gpu_int)}x GPUs found {res_gpu_int_arr}')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_support_bool] {str(res_gpu_int)}x GPUs found {res_gpu_int_arr}')
         return res_gpu_int_arr
     except Exception as e:
-        logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_device_count] Failed to get device_count. Using default [0]. Error: {e}')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [START] [cuda_device_count] Failed to get device_count. Using default [0]. Error: {e}')
         return [0]
 
 gpu_int_arr = cuda_device_count()
-
-# FastAPI app
 app = FastAPI()
-
-# Global LLM instance
 llm_instance = None
 
 @app.get("/")
@@ -64,11 +52,11 @@ async def root():
     return f'Hello from vllm server!'
 
 @app.post("/vllm")
-async def vllmt(request: Request):
+async def fnvllm(request: Request):
     global llm_instance
     try:
         req_data = await request.json()
-        logging.info(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [/vllmt] [req_data] {req_data}')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] [/vllm] [req_data] {req_data}')
         if req_data["req_type"] == "clear":
             try:
                 print(f' @ ********* CLEARING GPU MEMORY 0/3 *********')
@@ -80,7 +68,7 @@ async def vllmt(request: Request):
                 print(f' @ ********* CLEARING GPU MEMORY 3/3 gc.collect() *********')
                 print(f' @ ********* FINISHED *********')
             except Exception as e:
-                logging.error(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
+                print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
                 return JSONResponse({"result_status": 404, "result_data": str(e)})
         if req_data["req_type"] == "load":
             try:
@@ -144,34 +132,15 @@ async def vllmt(request: Request):
                 print(log_format.format("req_model_storage", "/models"))
                 print(log_format.format("req_model_path", "models/facebook/opt-125m"))
 
-                log_format = "{} >>>>>>>>>>>>>>>> {}: {}"
-                logging.info("")
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_type", req_data["req_type"]))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_model", "Qwen/Qwen2.5-1.5B-Instruct"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_tensor_parallel_size", "1"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_gpu_memory_utilization", "0.87"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_max_model_len", "2048"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_cpu_offload_gb", "0"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_enforce_eager", "True"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_enable_prefix_caching", "True"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_dtype", "auto"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_torch_dtype", "auto"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_kv_cache_dtype", "auto"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_swap_space", "4"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_enable_chunked_prefill", "True"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_trust_remote_code", "True"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_model_storage", "/models"))
-                logging.info(log_format.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "req_model_path", "models/facebook/opt-125m"))
-                logging.info("")
 
                 
                 
-                logging.info(f' @@@ searching {req_model_storage} ...')
+                print.info(f' @@@ searching {req_model_storage} ...')
                 models_found = []
                 try:                   
                     if os.path.isdir(req_model_storage):
-                        logging.info(f' @@@ found model storage path! {req_model_storage}')
-                        logging.info(f' @@@ getting folder elements ...')                        
+                        print.info(f' @@@ found model storage path! {req_model_storage}')
+                        print.info(f' @@@ getting folder elements ...')                        
                         for m_entry in os.listdir(req_model_storage):
                             m_path = os.path.join(req_model_storage, m_entry)
                             if os.path.isdir(m_path):
@@ -180,12 +149,12 @@ async def vllmt(request: Request):
                                     models_found.append(sub_item_path)        
                         print(f' @@@ found models ({len(models_found)}): {models_found}')
                     else:
-                        logging.info(f' @@@ ERR model path not found! {req_model_storage}')
+                        print.info(f' @@@ ERR model path not found! {req_model_storage}')
                 except Exception as e:
-                    logging.info(f' @@@ ERR getting models in {req_model_storage}: {e}')
+                    print.info(f' @@@ ERR getting models in {req_model_storage}: {e}')
 
                 
-                logging.info(f' @@@ does requested model path match downloaded?')
+                print.info(f' @@@ does requested model path match downloaded?')
                 model_path = req_model
                 if req_model_path in models_found:
                     print(f' @@@ FOUND MODELS ALREADY!!! {req_model} ist in {models_found}')
@@ -193,14 +162,8 @@ async def vllmt(request: Request):
                 else:
                     print(f' @@@ NUH UH DIDNT FIND MODEL YET!! {req_model} ist NAWT in {models_found}')
                 
-                # free_memory = torch.cuda.mem_get_info()[0] / (1024 ** 3)
-                # logging.info(f' @@@ Available GPU memory: {free_memory:.2f} GB')
-
-                # model_pretrained = AutoModelForCausalLM.from_pretrained(req_model, torch_dtype="auto")
-                # model_size = sum(p.numel() * p.element_size() for p in model_pretrained.parameters()) / (1024 ** 3)  # Size in GB
-                # logging.info(f' @@@ Model size: {model_size:.2f} GB')
                 
-                logging.info(f' @@@ Using model path: {model_path}')
+                print(f' @@@ Using model path: {model_path}')
 
                 llm_instance = LLM(
                     model=model_path,
@@ -216,11 +179,11 @@ async def vllmt(request: Request):
                     enable_chunked_prefill=req_enable_chunked_prefill,
                     trust_remote_code=req_trust_remote_code
                 )
-                logging.info(f' @@@ Model loaded successfully')
+                print(f' @@@ Model loaded successfully')
                 return JSONResponse({"result_status": 200, "result_data": "Model loaded successfully"})
 
             except Exception as e:
-                logging.error(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
+                print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
                 return JSONResponse({"result_status": 404, "result_data": str(e)})
 
         elif req_data["req_type"] == "generate":
@@ -232,21 +195,14 @@ async def vllmt(request: Request):
                 temperature = req_data.get("temperature", 0.8)
                 top_p = req_data.get("top_p", 0.95)
                 max_tokens = req_data.get("max_tokens", 100)
-                # print('')
-                # log_format = " >>>>>>>>>>>>>>>> {}: {}"
-                # print(log_format.format("prompt", prompt))
-                # print(log_format.format("temperature", temperature))
-                # print(log_format.format("top_p", top_p))
-                # print(log_format.format("max_tokens", max_tokens))
-                # print('')
-                
+                print('')
                 log_format = " >>>>>>>>>>>>>>>> {}: {}"
-                logging.info("")
-                logging.info(log_format.format("prompt", prompt))
-                logging.info(log_format.format("temperature", temperature))
-                logging.info(log_format.format("top_p", top_p))
-                logging.info(log_format.format("max_tokens", max_tokens))
-                logging.info("")
+                print(log_format.format("prompt", prompt))
+                print(log_format.format("temperature", temperature))
+                print(log_format.format("top_p", top_p))
+                print(log_format.format("max_tokens", max_tokens))
+                print('')
+
                 
                 
                 sampling_params = SamplingParams(
@@ -255,22 +211,22 @@ async def vllmt(request: Request):
                     max_tokens=max_tokens
                 )
 
-                logging.info(f'Generating text for prompt: {prompt}')
+                print(f'Generating text for prompt: {prompt}')
                 outputs = llm_instance.generate([prompt], sampling_params)
                 generated_text = outputs[0].outputs[0].text
-                logging.info(log_format.format("generated_text", generated_text[1:]))
+                print(log_format.format("generated_text", generated_text[1:]))
             
                 return JSONResponse({"result_status": 200, "result_data": generated_text[1:]})
 
             except Exception as e:
-                logging.error(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
+                print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
                 return JSONResponse({"result_status": 500, "result_data": str(e)})
 
         else:
             raise HTTPException(status_code=400, detail="Invalid request type.")
 
     except Exception as e:
-        logging.error(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
+        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] {e}')
         return JSONResponse({"result_status": 500, "result_data": str(e)})
 
 
